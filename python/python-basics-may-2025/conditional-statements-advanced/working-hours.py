1584ccd5d0a5512cbbccb9ed7951ad8e1401fb91
Valeri Elashvili <valerielashvili@gmail.com>
2025-05-26 22:25:30 +0200

    Correct style

--- python/python-basics-may-2025/conditional-statements-advanced/working-hours.py
+++ python/python-basics-may-2025/conditional-statements-advanced/working-hours.py
@@ -3,7 +3,8 @@ day = input()
 
 working_status = 'closed'
 
-if 10 <= hour <= 18 and (day == 'Monday' or day =='Tuesday' or day =='Wednesday' or day =='Thursday' or day =='Friday' or day =='Saturday'):
+if 10 <= hour <= 18 and (day == 'Monday' or day =='Tuesday' or day =='Wednesday' or day =='Thursday' or day =='Friday'
+                         or day =='Saturday'):
         working_status = 'open'
 
 print(working_status)
