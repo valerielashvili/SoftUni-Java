0c347784c517b1a46c32d32c8a96db0058a3181f
Valeri Elashvili <valerielashvili@gmail.com>
2025-06-21 02:02:40 +0200

    Add Coins

---
+++ python/python-basics-may-2025/while-loop/coins.py
@@ -0,0 +1,14 @@
+from math import floor
+
+change = float(input())
+cents = floor(change * 100)
+total_coins = 0
+
+for i in 200, 100, 50, 20, 10, 5, 2, 1:
+    if cents >= i:
+        coins = 0
+        coins += cents // i
+        cents -= coins * i
+        total_coins += coins
+
+print(int(total_coins))
